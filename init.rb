4464c87dc3b84bb0df7244b38d71e9a15adc13a8
koryo <koryobewakwakcom@gmail.com>
2014-12-12 21:03:22 +0900

    first commit

---
+++ init.rb
@@ -0,0 +1,7 @@
+Redmine::Plugin.register :k_chart do
+  name 'K Chart plugin'
+  author 'koryo'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+end
