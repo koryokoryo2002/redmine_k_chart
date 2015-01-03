35996db04f0b0bc1fcac38f06f267ae5264cf85a
koryo <koryobewakwakcom@gmail.com>
2015-01-03 23:13:00 +0900

    変数からのグラフ作成

--- app/controllers/k_chart_controller.rb
+++ app/controllers/k_chart_controller.rb
@@ -7,5 +7,25 @@ class KChartController < ApplicationController
 
   def velocity
     @project = Project.find(params[:project_id])
+
+    @version_names = []
+    @bug_issues_num = []
+    @dep_issues_num = []
+    idx = 0
+
+    @project.versions.each do|version|
+      @version_names.push version.name
+      @bug_issues_num.push 0
+      @dep_issues_num.push 0
+      
+      version.fixed_issues.each do|issue|
+        if issue.tracker_id == 2
+          @dep_issues_num[idx] += 1
+        else
+          @bug_issues_num[idx] += 1
+        end
+      end
+      idx =+ 1
+    end
   end
 end
